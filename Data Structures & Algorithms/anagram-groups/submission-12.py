501649775e0b08270205bfd3a2a65a364759c4f7
akshaj sunil <45850532+akshajsunil@users.noreply.github.com>
2026-09-07 22:51:56 +0530

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findhash(self,strs):
+        t=0
+        for i in strs:
+            t+=(ord(i)-ord("a")+23)**5
+        return t
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        for i in strs:
+            t=self.findhash(i)
+            #print(t)
+            if t not in dic :
+                dic[t] = [i]
+            else:
+                dic[t].append(i)
+        l =[]
+        for i in dic:
+            l.append(dic[i])
+        return l
+
+        
